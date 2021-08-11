95635d2f10ceb93a8264c918b3a018ae8a502af7
Artemiy Denisov <dam_mek@mail.ru>
2021-08-11 18:44:18 +0700

    final commit

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from pyrogram import Client, filters, types
 from datetime import datetime
-from config import APP_API_HASH, APP_API_ID, CHANNEL_ID, DONOR_CHANNEL_ID
 import os
+# from config import APP_API_HASH, APP_API_ID, CHANNEL_ID, DONOR_CHANNEL_ID
 
-# api_id = os.getenv('APP_API_ID')
-# api_hash = os.getenv('APP_API_HASH')
-api_id = APP_API_ID
-api_hash = APP_API_HASH
-channel_id = CHANNEL_ID
-donor_id = DONOR_CHANNEL_ID
+api_id = os.getenv('APP_API_ID')
+api_hash = os.getenv('APP_API_HASH')
+channel_id = os.getenv('CHANNEL_ID')
+donor_id = os.getenv('DONOR_CHANNEL_ID')
+# api_id = APP_API_ID
+# api_hash = APP_API_HASH
+# channel_id = CHANNEL_ID
+# donor_id = DONOR_CHANNEL_ID
 
 app = Client('bot_python', api_id, api_hash)
 
 
-@app.on_message(filters.chat(int(donor_id)))
+# @app.on_message(filters.chat([donor_id, int(channel_id)]))
+@app.on_message(filters.chat([int(donor_id), int(channel_id)]))
 def get_post(_, message: types.Message):
+    print('Post was got')
     text = message.text
     if 'on Binance' in text and 'DOWN/' not in text and 'UP/' not in text:
         app.send_message(channel_id, text)
+        print('Message was sent to channel')
 
 
 if __name__ == '__main__':
